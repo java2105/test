f7171237f8440727a5b280e3785d229193622e6a
java2105 <99851196+java2105@users.noreply.github.com>
2022-02-17 11:06:23 +0800

    Add files via upload

---
+++ Schedule.java
@@ -0,0 +1,14 @@
+public class Schedule{
+	public static void main(String[] args){
+		System.out.println("\t\t*课程表*");
+		System.out.println("-------------------------------------------------");
+		System.out.println("星期\t\t早上\t\t\t下午");
+		System.out.println("-------------------------------------------------");
+		System.out.println("周一\t\t英语\t\t\t高数");
+		System.out.println("周二\t\t政治\t\t\t计算机网络");
+		System.out.println("周三\t\tC++\t\t\tJava");
+		System.out.println("周四\t\t数据结构\t\tJava");
+		System.out.println("周五\t\t数据库\t\t\t操作系统");
+		System.out.println("-------------------------------------------------");
+	}
+}
